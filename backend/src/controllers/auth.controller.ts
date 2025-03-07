69dee51caaa496ce9c8b19e087ae174463699566
tamammabroukeh <tamammb367@@gmail.com>
2025-03-07 18:32:59 +0300

    push the project

---
+++ backend/src/controllers/auth.controller.ts
@@ -0,0 +1,93 @@
+import { NextFunction, Request, Response } from "express";
+import { asyncHandler } from "../middlewares/asyncHandler.middleware";
+import { config } from "../config/app.config";
+import { registerSchema } from "../validation/auth.validation";
+import { HTTPSTATUS } from "../config/http.config";
+import { registerUserService } from "../services/auth.service";
+import passport from "passport";
+
+export const googleLoginCallback = asyncHandler(
+  async (req: Request, res: Response) => {
+    const currentWorkspace = req.user?.currentWorkspace;
+
+    if (!currentWorkspace) {
+      return res.redirect(
+        `${config.FRONTEND_GOOGLE_CALLBACK_URL}?status=failure`
+      );
+    }
+
+    return res.redirect(
+      `${config.FRONTEND_ORIGIN}/workspace/${currentWorkspace}`
+    );
+  }
+);
+
+export const registerUserController = asyncHandler(
+  async (req: Request, res: Response) => {
+    const body = registerSchema.parse({
+      ...req.body,
+    });
+    console.log(body);
+
+    await registerUserService(body);
+
+    return res.status(HTTPSTATUS.CREATED).json({
+      message: "User created successfully",
+    });
+  }
+);
+
+export const loginController = asyncHandler(
+  async (req: Request, res: Response, next: NextFunction) => {
+    console.log("ny");
+    passport.authenticate(
+      "local",
+      (
+        err: Error | null,
+        user: Express.User | false,
+        info: { message: string } | undefined
+      ) => {
+        if (err) {
+          console.log(err);
+          return next(err);
+        }
+
+        if (!user) {
+          console.log(info?.message);
+          return res.status(HTTPSTATUS.UNAUTHORIZED).json({
+            message: info?.message || "Invalid email or password",
+          });
+        }
+
+        req.logIn(user, (err) => {
+          if (err) {
+            return next(err);
+          }
+
+          return res.status(HTTPSTATUS.OK).json({
+            message: "Logged in successfully",
+            user,
+          });
+        });
+      }
+    )(req, res, next);
+  }
+);
+
+export const logOutController = asyncHandler(
+  async (req: Request, res: Response) => {
+    req.logout((err) => {
+      if (err) {
+        console.error("Logout error:", err);
+        return res
+          .status(HTTPSTATUS.INTERNAL_SERVER_ERROR)
+          .json({ error: "Failed to log out" });
+      }
+    });
+
+    req.session = null;
+    return res
+      .status(HTTPSTATUS.OK)
+      .json({ message: "Logged out successfully" });
+  }
+);
