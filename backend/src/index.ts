dae7928f7d6fd45030133d12c2f9c2f8122e6260
tamammabroukeh <tamammb367@@gmail.com>
2025-03-08 16:08:02 +0300

    fix secure and samesite

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -32,9 +32,9 @@ app.use(
     name: "session",
     keys: [config.SESSION_SECRET],
     maxAge: 24 * 60 * 60 * 1000,
-    secure: config.NODE_ENV === "production",
+    secure: config.NODE_ENV === "production" ? true : false,
     httpOnly: true,
-    sameSite: "lax",
+    sameSite: config.NODE_ENV === "production" ? "none" : "lax",
   })
 );
 
