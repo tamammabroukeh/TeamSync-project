e8ff18e199b2f3a89088135bf5d2f358037bb2c9
tamammabroukeh <tamammb367@@gmail.com>
2025-03-08 23:37:21 +0300

    fix session

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -32,9 +32,9 @@ app.use(
     name: "session",
     keys: [config.SESSION_SECRET],
     maxAge: 24 * 60 * 60 * 1000,
-    secure: config.NODE_ENV === "production" ? true : false,
+    secure: config.NODE_ENV === "production",
     httpOnly: true,
-    sameSite: config.NODE_ENV === "production" ? "none" : "lax",
+    sameSite: "none",
   })
 );
 
